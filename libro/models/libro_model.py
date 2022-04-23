f1ec75b676fb01a9145c84e2553ca84aeb592c27
José David Panaza Batres <3848221850101@ingenieria.usac.edu.gt>
2022-04-22 19:01:12 -0600

    actualizacion1

---
+++ libro/models/libro_model.py
@@ -0,0 +1,37 @@
+class Libro():
+    def __init__(self, isbn, autores, ypubli, titulo, edicion, temas, descrip, ncopias, estanteria, fila):
+        self.__isbn = isbn
+        self.__autores = autores
+        self.__ypubli = ypubli
+        self.__titulo = titulo
+        self.__edicion = edicion
+        self.__temas = temas
+        self.__descrip = descrip
+        self.__ncopias = ncopias
+        self.__estanteria = estanteria
+        self.__fila = fila
+        
+    def agregarcopias(self, cantidad):
+        if(cantidad>0):
+            self.__ncopias += cantidad
+        else:
+            return{
+                
+            }
+    def restarcopias(self):
+        self.__ncopias = self.__ncopias - 1
+    
+    
+    def getdatos(self):
+        return{
+            "isbn": self.__isbn,
+            "autores": self.__autores,
+            "ypubli": self.__ypubli,
+            "titulo": self.__titulo,
+            "edicion": self.__edicion,
+            "temas": self.__temas,
+            "descripcion": self.__descrip,
+            "ncopias": self.__ncopias,
+            "estanteria": self.__estanteria,
+            "fila": self.__fila
+        }
---
+++ prestamista/models/prestamista_model.py
@@ -0,0 +1,32 @@
+class Prestamista():
+    lprestado = False
+    def __init__(self, cui, nombre, apellido):
+        self.__cui = cui
+        self.__nombre = nombre
+        self.__apellido = apellido
+        
+    def prestarlibro(self):
+        global lprestado
+        lprestado = True
+    
+    def devolverlibro(self):
+        global lprestado
+        lprestado = False
+    
+    def getcui(self):
+        return self.__cui
+    
+    def getnombre(self):
+        return self.__nombre
+    
+    def getapellido(self):
+        return self.__apellido
+     
+    def getdatos(self):
+        return{
+            "cui": self.__cui,
+            "nombre": self.__nombre,
+            "apellido": self.__apellido            
+        }
+    
+    
